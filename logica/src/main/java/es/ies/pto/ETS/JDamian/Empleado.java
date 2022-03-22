2ac38d65e340d611851c1e878b88b777d5a51fa5
JDamianHdezMartin <damian.hdez.martin@gmail.com>
2022-03-22 19:48:09 +0000

    closed #7; Se implementan las clases del ddiagrama de clases

--- logica/src/main/java/es/ies/pto/ETS/JDamian/Empleado.java
+++ logica/src/main/java/es/ies/pto/ETS/JDamian/Empleado.java
@@ -12,5 +12,31 @@ public class Empleado {
         this.nombre = nombre;
     }
 
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
 
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public void anadirLibro(){
+
+    }
+    public void modificarLibro(){
+
+    }
+    public void eliminarLibro(){
+
+    }
+    public void eliminarUsuario(){
+
+    }
 }
--- logica/src/main/java/es/ies/pto/ETS/JDamian/Usuario.java
+++ logica/src/main/java/es/ies/pto/ETS/JDamian/Usuario.java
@@ -25,5 +25,52 @@ public class Usuario {
         this.contrasenia = contrasenia;
         this.correoElectrónico = correoElectrónico;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public int getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(int contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getCorreoElectrónico() {
+        return correoElectrónico;
+    }
+
+    public void setCorreoElectrónico(String correoElectrónico) {
+        this.correoElectrónico = correoElectrónico;
+    }
+    public void buscarLibro(){
+
+    }
+    public void registrarse(){
+
+    }
+    public void darseDeBaja(){
+
+    }
+    public void comprarLibro(){
+
+    }
+    public void solicitarDevolucion(){
+
+    }
 }
 
